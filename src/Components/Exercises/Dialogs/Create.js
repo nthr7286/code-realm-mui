de979d6947677236da9af27bc82ec2e338c41a12
natsuki <nthr7286@gmail.com>
2020-01-20 05:37:05 +0000

    Finish Lesson 05

---
+++ src/Components/Exercises/Dialogs/Create.js
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from 'react'
+import { Dialog, Button } from 'material-ui'
+import {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog'
+import { Add } from 'material-ui-icons'
+
+export default class extends Component {
+  state = {
+    open: false
+  }
+
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+  render() {
+
+    const { open } = this.state
+
+    return <Fragment>
+      <Button 
+        variant="fab" 
+        onClick={this.handleToggle}
+        mini
+      >
+        <Add />
+      </Button>
+      <Dialog
+        open={open}
+      >
+        <DialogTitle>
+          Create a New Exercise
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please fill out the form below.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button 
+            color="primary"
+            variant="raised"
+          >
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Fragment>
+  }
+}
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography } from 'material-ui'
+import CreateDialog from '../Exercises/Dialogs/Create'
 
 export default props =>
 <AppBar position="static">
   <Toolbar>
-    <Typography variant="headline" color="inherit">
+    <Typography 
+      variant="headline" 
+      color="inherit"
+      style={{flex: 1}}
+    >
       Exerceise Database
     </Typography>
+    <CreateDialog />
   </Toolbar>
 </AppBar>
